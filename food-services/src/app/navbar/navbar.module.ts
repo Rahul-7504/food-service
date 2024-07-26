1592e9261f6a392191fe62e69ade6f902c0bd8ec
giteshiva44 <giteshiva44@gmail.com>
2024-07-26 15:17:28 +0530

    create project structure

---
+++ food-services/src/app/navbar/navbar.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavbarRoutingModule } from './navbar-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
+import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    ContactUsComponent,
+    LoginComponent,
+    AddRestaurantComponent
+  ],
+  imports: [
+    CommonModule,
+    NavbarRoutingModule
+  ]
+})
+export class NavbarModule { }
