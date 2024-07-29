8974a58b6bbacbe167d3062534934e7595a0195d
giteshiva44 <giteshiva44@gmail.com>
2024-07-29 17:17:39 +0530

    add new code

--- food-services/src/app/app-routing.module.ts
+++ food-services/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'navbar/home', pathMatch: 'full' },
   { path: 'navbar', loadChildren: () => import('./navbar/navbar.module').then(m => m.NavbarModule) },
+  { path: '', redirectTo: 'navbar/login', pathMatch: 'full' },
  
 ];
 
