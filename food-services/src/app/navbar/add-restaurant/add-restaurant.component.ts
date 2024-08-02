6038bc0691038b00e0bb8ff2f3c1cf9fb2c7b68b
vaibhav godage <vaibhavgodage8@gmail.com>
2024-08-02 12:43:34 +0530

    update code

--- food-services/src/app/navbar/add-restaurant/add-restaurant.component.ts
+++ food-services/src/app/navbar/add-restaurant/add-restaurant.component.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-add-restaurant',
   templateUrl: './add-restaurant.component.html',
   styleUrls: ['./add-restaurant.component.scss']
 })
 export class AddRestaurantComponent {
+
   loginForm: any = FormGroup;
   constructor(private fb:FormBuilder){
     this.loginForm=this.fb.group({
-      ownerName: ['', Validators.required],
-      hotelName: ['', Validators.required],
-      hostelName: ['', Validators.required],
-      hotel: ['', Validators.required],
+      ownerName: ['',  [Validators.required, Validators.pattern('^[A-Za-z ]*$'), Validators.minLength(2)]],
+      hotelName: ['', Validators.required, Validators.pattern('^[A-Za-z ]*$')],
+      hostelName: ['', Validators.required, Validators.pattern('^[A-Za-z ]*$')],
+      hotel: ['', Validators.required, Validators.pattern('^[A-Za-z ]*$')],
       contactNumber: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
-      hotelRegisterNumber: ['', Validators.required],
+      hotelRegisterNumber: ['', Validators.required,Validators.pattern('^[0-9]*$')],
     })
   }
   onsubmit(){
     this.loginForm.value;
   }
+  preventNumericInput(event: KeyboardEvent) {
+    const charCode = event.charCode;
+    if (charCode >= 48 && charCode <= 57) { // ASCII codes for 0-9
+      event.preventDefault();
+    }
+  }
 
 }
+
