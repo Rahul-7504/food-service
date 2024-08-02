168f753a349424e835bdeba8010fae9663b7bddf
Vishal Sanap <vishalsanap897@gmail.com>
2024-08-02 18:05:50 +0530

    changes in login page

--- food-services/src/app/navbar/login/login.component.ts
+++ food-services/src/app/navbar/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,18 +7,24 @@ import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  loginForm:FormGroup;
+  loginForm: FormGroup;
   submittedloginForm: boolean = false;
 
-  constructor(private fb:FormBuilder){
-    this. loginForm = this.fb.group({
-      phonenumber:['',Validators.required]
-    })
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      phonenumber: ['', Validators.required]
+    });
   }
-  onSubmit(){
-    this. submittedloginForm = true;
+
+  onSubmit() {
+    this.submittedloginForm = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+  
   }
-  get f(){
+
+  get f() {
     return this.loginForm.controls;
   }
 }
