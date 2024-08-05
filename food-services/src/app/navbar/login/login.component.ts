dbdec25ba7439809f724c1354f2925e1b54e3a6d
Vishal Sanap <vishalsanap897@gmail.com>
2024-08-05 12:06:19 +0530

    correct changes in login page

--- food-services/src/app/navbar/login/login.component.ts
+++ food-services/src/app/navbar/login/login.component.ts
@@ -9,22 +9,45 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent {
   loginForm: FormGroup;
   submittedloginForm: boolean = false;
+  isHideshow: boolean = false;
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
-      phonenumber: ['', Validators.required]
+      phonenumber: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
     });
   }
 
   onSubmit() {
+    // this.isHideshow = true;
     this.submittedloginForm = true;
-    if (this.loginForm.invalid) {
-      return;
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
     }
-  
   }
 
-  get f() {
-    return this.loginForm.controls;
+
+  changePhoneNumber(){
+    if(this.phonenumber?.valid){
+      this.isHideshow=true;
+    }else{
+      this.isHideshow=false;
+    }
+  }
+
+
+  get phonenumber() {
+    return this.loginForm.get('phonenumber');
+  }
+
+  onDigit(event: any, previous: any, next: any): void {
+    const input = event.target.value;
+    if (/\d/.test(input)) {
+      if (next !== null) {
+        next.focus();
+      }
+    } else if (event.code === 'Backspace' && previous !== null) {
+      previous.focus();
+      previous.value = '';
+    }
   }
 }
