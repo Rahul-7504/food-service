9b3f50412146af3c64019b4c485a91c28431a480
navkesh <navkeshlohate01@gmail.com>
2024-08-05 11:49:11 +0530

    create form

--- food-services/src/app/navbar/food-template/food-template.component.ts
+++ food-services/src/app/navbar/food-template/food-template.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-food-template',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./food-template.component.scss']
 })
 export class FoodTemplateComponent {
+ 
+    
+  productForm:any=FormGroup
+  constructor(private fb:FormBuilder){
+    this.productForm=this.fb.group({
+      productName:['',Validators.required],
+      productPrice:['',Validators.required]
+      
+    })
+  }
+  submitted=false
+  get formOrder(){
+      return this.productForm.controls;
+  }
+  saveData(){
+    this.submitted=true
+    console.log(this.productForm.value);
+  }
+  clearData(){
 
+  }
+  
 }
