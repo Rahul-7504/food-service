0b438fa608c048c8c14a0a5780ce09574b6accb1
vaibhav godage <vaibhavgodage8@gmail.com>
2024-08-13 11:59:28 +0530

    work on ordernow component

--- food-services/src/app/navbar/order-now/order-now.component.ts
+++ food-services/src/app/navbar/order-now/order-now.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-order-now',
   templateUrl: './order-now.component.html',
@@ -13,7 +13,7 @@ export class OrderNowComponent {
   selectedplanDetails: undefined;
   memberitems:any =[{},];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router ,private translate: TranslateService) { }
 
   closeSiderbar(event:any){
     if(event){
